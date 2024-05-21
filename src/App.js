11f5b5ed7d8320afe55f79fafa97be08f4d34957
shmewy <167490786+shmewy@users.noreply.github.com>
2024-05-20 21:34:02 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ import Channel from './components/Channel';
 import Loader from './components/Loader';
 
 firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_AUTH_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyB4UN3nst1WLILNcLnjPmrfwAHcgpmo6zM",
+  authDomain: "lunaticchatroom.firebaseapp.com",
+  projectId: "lunaticchatroom",
+  storageBucket: "lunaticchatroom.appspot.com",
+  messagingSenderId: "1015861532385",
+  appId: "1:1015861532385:web:6622ca215026250972c9a0",
 });
 
 const MoonIcon = props => (
